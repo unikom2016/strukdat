f2542c1561ab3488a804e9d5ca661e0cd5420087
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-04-05 13:52:40 +0700

    [Add] 3 procedures - Create, Destroy, Add data, Insertion at first (not fully implemented)

--- week5/practice_doubly_linked_list.pas
+++ week5/practice_doubly_linked_list.pas
@@ -11,11 +11,11 @@ type
     info       : mahasiswa;
     prev, next : point;
   end;
-
 { Kamus Global }
 var
   head, tail : point;
-  elemen     : string; // as nim temporary
+  elem     : string; // as nim temporary
+  again      : char; // to hold when user needs to another data
 
 
 { Create }
@@ -25,14 +25,49 @@ begin
   tail := nil;
 end;
 
+{ Addition }
+procedure addData(var elem : string)
+begin
+  write('Masukkan NIM   : '); readln(elem);
+  // write('Masukkan Nama  : '); readln(elem);
+end;
+
+{ Insertion }
+procedure addAtFirst()
+begin
+  new(temp);
+  temp^.prev := nil;
+  temp^.info := dmhs;
+  temp^.next := head;
+
+end;
 
 { Destroy }
 procedure destroy(var head, tail : point);
+var
+  temp : point;
 begin
-  head := nil;
+  new(temp); temp := head;
+  while (temp <> nil) do
+  begin
+    head := head^.next;
+    dispose(temp);
+    temp := head;
+  end;
   tail := nil;
 end;
 
 begin
   
+  repeat
+    addData(elem);
+    write('Masukkan lagi? (Y/T)'); readln(again);
+  until (upcase(again) = 'T');
+
+  destroy(head, tail);
+  if (head = nil) then
+  begin
+    writeln('Data kosong!');
+    readln;
+  end;
 end.
